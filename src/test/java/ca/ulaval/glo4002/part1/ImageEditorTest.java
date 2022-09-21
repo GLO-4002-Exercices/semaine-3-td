c0d6fcd297d1ef8521abac7d516e37ec597fbc2f
anthonyblanchettepotvin <anthony.blanchette.potvin@gmail.com>
2022-09-21 19:04:12 -0400

    part1

--- src/test/java/ca/ulaval/glo4002/part1/ImageEditorTest.java
+++ src/test/java/ca/ulaval/glo4002/part1/ImageEditorTest.java
@@ -1,27 +1,47 @@
 package ca.ulaval.glo4002.part1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class ImageEditorTest {
     private static final String AN_IMAGE = "potato";
+    private static final String EXPECTED_PATH = "/tmp/part1-images/potato.png";
 
     private ImageEditor imageEditor;
 
     @Test
     void whenLoadingAnImage_thenLoadsImageFromTheRightDirectory() {
-        imageEditor = new ImageEditor(null /* new ResourcesImageLoader() ?? */);
+        ImageLoaderDouble imageLoader = new ImageLoaderDouble();
+        imageEditor = new ImageEditor(imageLoader);
 
         imageEditor.renderImageByName(AN_IMAGE);
 
-        // ???, sans mockito
+        Assertions.assertTrue(imageLoader.wasLoadCalled);
+        Assertions.assertEquals(EXPECTED_PATH, imageLoader.loadCallParameter);
     }
 
     @Test
     void whenLoadingAnImage_thenLoadsImageFromTheRightDirectory_withMockito() {
-        imageEditor = new ImageEditor(null /* new ResourcesImageLoader() ?? */);
+        ResourcesImageLoader resourcesImageLoader = Mockito.mock(ResourcesImageLoader.class);
+        imageEditor = new ImageEditor(resourcesImageLoader);
 
         imageEditor.renderImageByName(AN_IMAGE);
 
-        // verify(...)
+        Mockito.verify(resourcesImageLoader).load(EXPECTED_PATH);
+    }
+
+    private static class ImageLoaderDouble implements ImageLoader {
+
+        public boolean wasLoadCalled = false;
+        public String loadCallParameter = "";
+
+        @Override
+        public byte[] load(String path) {
+            wasLoadCalled = true;
+            loadCallParameter = path;
+
+            return new byte[]{};
+        }
     }
 }
